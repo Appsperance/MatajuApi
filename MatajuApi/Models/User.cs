4e057e3a0ae198802cf75750fd7489a20997e867
Kyle KIM <Kyle4Open@gmail.com>
2024-12-11 01:41:22 +0900

    add: 회원가입 엔드포인트 추가 [api/user/register]

--- MatajuApi/Models/User.cs
+++ MatajuApi/Models/User.cs
@@ -13,25 +13,20 @@ namespace MatajuApi.Models
         [Column("UserId")]
         public int Id { get; set; }
 
-        [Required]
         public string Name { get; set; } = string.Empty;
 
         /// <summary>
         ///  password + salt가 해싱된 Base64 문자열
         /// </summary>
-        [Required]
         public string Password { get; set; } = string.Empty;
 
         /// <summary>
         ///  해싱되기 전에 password 뒤에 추가된 24글자 문자열(base64)
         /// </summary>
-        [Required]
         public string Salt { get; set; } = string.Empty;
 
-        [Required]
         public string Nickname { get; set; } = string.Empty;
 
-        [Required]
         public string Roles { get; set; } = "user";
     }
 }
