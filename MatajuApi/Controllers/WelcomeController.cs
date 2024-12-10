659931f3152e4c353e12be4c1b27abe6b6e24d73
Kyle KIM <Kyle4Open@gmail.com>
2024-12-11 00:33:01 +0900

    feat: 환영 API 엔드포인트 추가 [/api]

---
+++ MatajuApi/Controllers/WelcomeController.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace MatajuApi.Controllers
+{
+    [ApiController]
+    [Route("api")]
+    public class WelcomeController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Welcome()
+        {
+            return Ok("어서 와유~ '마타유' API 백엔드가 실행중이구유. 뭘 맡아드릴까유?");
+        }
+    }
+}
